c33c4e8671610de3209fc5deb98f9fbcba60fa66
Sammaeliusz <93608787+Sammaeliusz@users.noreply.github.com>
2025-10-12 16:15:28 +0200

    Add files via upload

--- zero.m
+++ zero.m
@@ -1,14 +1,14 @@
-blades = 5;
-x = 0:pi/100:2*pi;
+blades = 6
+x = 0:pi/100:4*pi;
 frame = getframe(gcf).cdata;
 for s = 120:10:200
     for l = 1:1:16
         o=0;
         for m = 2:1:size(frame, 1)
-            f = sin(blades*x+m*pi/s);
+            f = sin((blades/2)*x+m*pi/s).^2;
             polarplot(x,f);
             hold on
-            f = sin(-1*(blades-2)*x+m*pi/s*1.75+pi/3);
+            f = sin(-1*(blades/2-1)*x+m*pi/s*1.75+pi/3).^2;
             polarplot(x,f,'g');
             hold off
             for n = 1:1:l
@@ -18,6 +18,6 @@ for s = 120:10:200
             end
             o=o+l;
         end
-        imwrite(frame,"35."+l+"."+s+".png");
+        %imwrite(frame,"35."+l+"."+s+".png");
     end
 end
